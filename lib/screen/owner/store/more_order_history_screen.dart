47e376b6996e5ed20c87abc88fc963c1d6b48fcf
Firlana Saddam Pramadhan <firlanasaddamp@gmail.com>
2024-07-10 19:13:11 +0700

    <feat>[Order] : add see more history functionality

--- lib/screen/owner/store/more_order_history_screen.dart
+++ lib/screen/owner/store/more_order_history_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:tugas_akhir_app/provider/order_history_provider.dart';
+import 'package:tugas_akhir_app/screen/widgets/card_history.dart';
 
 class MoreOrderHistoryScreen extends StatefulWidget {
   const MoreOrderHistoryScreen({super.key, required this.storeId});
@@ -16,8 +20,43 @@ class _MoreOrderHistoryScreenState extends State<MoreOrderHistoryScreen> {
       appBar: AppBar(
         title: const Text('Order History'),
       ),
-      body: const Center(
-        child: Text('History'),
+      body: RefreshIndicator(
+        onRefresh: () => context
+            .read<OrderHistoryProvider>()
+            .refreshOrderHistory(storeId: widget.storeId),
+        child: Consumer<OrderHistoryProvider>(
+            builder: (context, historyProvider, _) {
+          final state = historyProvider.loadingState;
+          return state.when(
+              initial: () => const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+              loading: () => const Center(
+                    child: CircularProgressIndicator(),
+                  ),
+              loaded: () {
+                return ListView.builder(
+                  itemCount: historyProvider.orderHistories.length,
+                  itemBuilder: (context, index) {
+                    return CardHistory(
+                      history: historyProvider.orderHistories[index],
+                      onTap: () {
+                        context.goNamed('detail_order', pathParameters: {
+                          'id': widget.storeId,
+                          'orderId':
+                              historyProvider.orderHistories[index].orderId
+                        });
+                      },
+                    );
+                  },
+                );
+              },
+              error: (error) {
+                return Center(
+                  child: Text(error),
+                );
+              });
+        }),
       ),
     );
   }
